a1fbc58119cadab5b11efeda4d9f51a39e9d218b
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-05 18:42:40 -0600

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { PrimeReactProvider } from 'primereact/api';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from "./App";
 
 import "primereact/resources/themes/mdc-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "./styles.css";
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <QueryClientProvider client={queryClient}>
+      <PrimeReactProvider>
+        <App />
+        <ReactQueryDevtools />
+      </PrimeReactProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
