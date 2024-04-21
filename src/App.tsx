971f3f0aa884b3a98c5b16dc882372ed332925be
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-20 23:44:30 -0600

    refactor context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppContent, AppProvider } from "./AppContext";
+import { PrimeReactProvider } from "primereact/api";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <AppProvider>
-      <AppContent />
+      <QueryClientProvider client={queryClient}>
+        <PrimeReactProvider>
+          <AppContent />
+          <ReactQueryDevtools />
+        </PrimeReactProvider>
+      </QueryClientProvider>
     </AppProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PrimeReactProvider } from "primereact/api";
-import {
-  QueryClient,
-  QueryClientProvider,
-  // useQuery,
-} from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 
 import "primereact/resources/themes/mdc-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 import "./styles.css";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <PrimeReactProvider>
-        <App />
-        <ReactQueryDevtools />
-      </PrimeReactProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
