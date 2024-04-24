09756725fcd096a037fd24cef2f24795bc20290a
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-23 23:36:24 -0600

    db container deploying with pre-generated csv

---
+++ dev_db/db_init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS guests (
+    guest_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    town VARCHAR(100),
+    notes TEXT,
+    creator VARCHAR(100),
+    creation_time INT
+);
+
+CREATE TABLE IF NOT EXISTS passes (
+    pass_id SERIAL PRIMARY KEY,
+    guest_id INT REFERENCES guests(guest_id),
+    passtype VARCHAR(50),
+    remaining_uses INT,
+    active BOOLEAN,
+    payment_method VARCHAR(50),
+    amount_paid_cents INT,
+    creator VARCHAR(100),
+    creation_time INT
+);
+
+COPY guests FROM '/app/dummy_guests.csv' DELIMITER ',' CSV;
+COPY passes FROM '/app/dummy_passes.csv' DELIMITER ',' CSV;
--- dev_db/dummy_data.py
+++ dev_db/dummy_data.py
@@ -1,6 +1,6 @@
 import random
 import uuid
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Any
 
 INIT_DB_SQL_STRING: str = r""" CREATE TABLE IF NOT EXISTS guests (
@@ -69,6 +69,12 @@ class Guest:
     creator: str
     creation_time: int
 
+    def header_string(self, delimiter: str) -> str:
+        return delimiter.join(str(k) for k in asdict(self).keys())
+
+    def values_string(self, delimiter: str) -> str:
+        return delimiter.join(str(v) for v in asdict(self).values())
+
 
 @dataclass
 class PunchPass:
@@ -82,12 +88,42 @@ class PunchPass:
     creator: str
     creation_time: int
 
+    def header_string(self, delimiter: str) -> str:
+        return delimiter.join(str(k) for k in asdict(self).keys())
+
+    def values_string(self, delimiter: str) -> str:
+        return delimiter.join(str(v) for v in asdict(self).values())
+
+
+def main():
+    old_first_names = read_names("old_first_names.txt")
+    new_first_names = read_names("new_first_names.txt")
+    last_names = read_names("last_names.txt")[:200]
+
+    first_names = old_first_names + new_first_names
+
+    guests = generate_guests(20_000, first_names, last_names)
+    passes = generate_passes(guests, first_names)
+
+    write_csv(guests, "dummy_guests.csv")
+    write_csv(passes, "dummy_passes.csv")
+
+    # print_db_init_sql(guests, passes)
+
+
+def write_csv(data: list[Guest | PunchPass], filename):
+    with open(filename, "w") as file:
+        for item in data:
+            file.write(item.values_string(","))
+            file.write("\n")
+
 
 def generate_guests(
     num: int, first_names: list[str], last_names: list[str]
 ) -> list[Guest]:
     guests = []
-    for i in range(10_000):
+    for i in range(num):
+        guest_id = i + 100_000
         first_name = random.choice(first_names)
         last_name = random.choice(last_names)
         email = f"{first_name.lower()}.{last_name.lower()}@email.com"
@@ -96,7 +132,7 @@ def generate_guests(
         creator = f"{random.choice(first_names[:24])}"
         creation_time = random.randrange(1613755541, 1713755541)
         guest = Guest(
-            i + 100_000,
+            guest_id,
             first_name,
             last_name,
             email,
@@ -110,9 +146,7 @@ def generate_guests(
     return guests
 
 
-def generate_passes(
-    guests: list[Guest], first_names: list[str], last_names: list[str]
-) -> list[PunchPass]:
+def generate_passes(guests: list[Guest], first_names: list[str]) -> list[PunchPass]:
     passes = []
     for i in range(len(guests) * 2):
         passholder = random.choice(guests)
@@ -142,53 +176,31 @@ def generate_passes(
     return passes
 
 
-def output_sql_insert_guest(guests: list[Guest]):
-    header = """INSERT INTO guests (guest_id, first_name, last_name, email, town, notes, creator, creation_time)
+def print_sql_insert(
+    data: list[PunchPass | Guest],
+    table: str,
+):
+    header = f"""INSERT INTO {table} ({data[0].sql_header_string()})
 VALUES"""
     print(header)
-    for guest in guests[:-1]:
-        values = f"""({guest.guest_id}, '{guest.first_name}', '{guest.last_name}', '{guest.email}', '{guest.town}', '{guest.notes}', '{guest.creator}', {guest.creation_time}),"""
+    for p in data[:-1]:
+        values = f"""({p.values_string(", ")}),"""
         print(values)
-    guest = guests[-1]
-    footer = f"""({guest.guest_id}, '{guest.first_name}', '{guest.last_name}', '{guest.email}', '{guest.town}', '{guest.notes}', '{guest.creator}', {guest.creation_time});"""
+    p = data[-1]
+    footer = f"""({p.values_string(", ")});"""
     print(footer)
 
 
-def output_sql_insert_pass(passes: list[PunchPass]):
-    header = """INSERT INTO passes (pass_id, guest_id, passtype, remaining_uses, active, payment_method, amount_paid_cents, creator, creation_time)
-VALUES"""
-    print(header)
-    for p in passes[:-1]:
-        values = f"""({p.pass_id}, {p.guest_id}, '{p.passtype}', {p.remaining_uses}, {p.active}, '{p.payment_method}', {p.amount_paid_cents}, '{p.creator}', {p.creation_time}),"""
-        print(values)
-    p = passes[-1]
-    footer = f"""({p.pass_id}, {p.guest_id}, '{p.passtype}', {p.remaining_uses}, {p.active}, '{p.payment_method}', {p.amount_paid_cents}, '{p.creator}', {p.creation_time});"""
-    print(footer)
-
-
-def output_db_init_sql(guests: list[Guest], passes: list[PunchPass]):
+def print_db_init_sql(guests: list[Guest], passes: list[PunchPass]):
     print(INIT_DB_SQL_STRING)
-    output_sql_insert_guest(guests)
+    print_sql_insert(guests, "guests")
     print()
-    output_sql_insert_pass(passes)
+    print_sql_insert(passes, "passes")
 
 
-def main():
-    with open("old_first_names.txt", "r") as file:
-        old_first_names = [name.strip() for name in file.readlines()]
-
-    with open("new_first_names.txt", "r") as file:
-        new_first_names = [name.strip() for name in file.readlines()]
-
-    with open("last_names.txt", "r") as file:
-        last_names = [name.strip() for name in file.readlines()][:200]
-
-    first_names = old_first_names + new_first_names
-
-    guests = generate_guests(10_000, first_names, last_names)
-    passes = generate_passes(guests, first_names, last_names)
-
-    output_db_init_sql(guests, passes)
+def read_names(filename: str) -> list[str]:
+    with open(filename, "r") as file:
+        return [name.strip() for name in file.readlines()]
 
 
 if __name__ == "__main__":
