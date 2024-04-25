1566f46a88756205df5b22827b5acd29e040dc4d
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-24 19:35:44 -0600

    rename

---
+++ src/screens/ViewGuest.tsx
@@ -0,0 +1,24 @@
+import { Button } from "primereact/button";
+
+import { Screen } from "../types";
+import { PassInfo } from "../components/PassInfo";
+import { useAppContext } from "../AppContext";
+
+export function ViewGuest() {
+  const { setScreen } = useAppContext();
+
+  return (
+    <div className="viewport-wrapper">
+      <div className="center-box">
+        <PassInfo />
+        <Button
+          label="Back"
+          onClick={(e) => {
+            e.preventDefault();
+            setScreen(Screen.Dashboard);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
