9bc21ec2a5b909c5008f42db67c8f7a76ec8068f
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-26 14:59:23 -0600

    formatting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,15 +13,21 @@ pub mod api;
 pub mod database;
 pub mod queries;
 
-use api::{add_visits, async_sleep, create_pass, toggle_pass_active, delete_pass, get_guest, log_visit, search_passes};
-
-// const PG_CONNECT_STRING: &str = "postgres://postgres:joyful@172.22.0.22/passtracker-dev";
-// const PG_CONNECT_STRING: &str = "postgres://postgres:joyful_journey@35.247.29.177/passtracker";
+use api::{
+    add_visits, async_sleep, create_pass, delete_pass, get_guest, log_visit, search_passes,
+    toggle_pass_active,
+};
 
 const USERNAME: &str = "postgres";
 const PASSWORD: &str = "joyful";
 const DB_HOST: &str = "172.22.0.22";
 const DB_NAME: &str = "passtracker-dev";
+
+// const USERNAME: &str = "postgres";
+// const PASSWORD: &str = "joyful_journey";
+// const DB_HOST: &str = "35.247.29.177";
+// const DB_NAME: &str = "passtracker";
+
 
 pub struct AppState {
     pg_pool: Arc<Mutex<PgPool>>,
