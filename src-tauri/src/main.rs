98c946f770ad85dcbe5b74cf13388c02070f30bd
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-04-26 17:06:33 -0600

    disabled window menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,8 @@ use sqlx::{
     postgres::{PgConnectOptions, PgPoolOptions},
     PgPool,
 };
-use tauri::{async_runtime::Mutex, CustomMenuItem, Menu, MenuItem, Submenu};
+use tauri::async_runtime::Mutex;
+// use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 pub mod api;
 pub mod database;
@@ -27,7 +28,6 @@ const DB_NAME: &str = "passtracker-dev";
 // const PASSWORD: &str = "joyful_journey";
 // const DB_HOST: &str = "35.247.29.177";
 // const DB_NAME: &str = "passtracker";
-
 
 pub struct AppState {
     pg_pool: Arc<Mutex<PgPool>>,
@@ -55,25 +55,25 @@ async fn main() {
         pg_pool: Arc::new(Mutex::new(pg_pool)),
     };
 
-    let dashboard = CustomMenuItem::new("dashboard".to_string(), "Dashboard");
-    let settings = CustomMenuItem::new("settings".to_string(), "Settings...");
-    // let close = CustomMenuItem::new("quit".to_string(), "Quit");
-    let about = CustomMenuItem::new("about".to_string(), "About...");
-    let submenu = Submenu::new(
-        "File",
-        Menu::new()
-            .add_item(dashboard)
-            .add_item(settings)
-            .add_native_item(MenuItem::Separator)
-            .add_item(about),
-    );
-    let menu = Menu::new()
-        .add_native_item(MenuItem::Copy)
-        .add_submenu(submenu);
+    // let dashboard = CustomMenuItem::new("dashboard".to_string(), "Dashboard");
+    // let settings = CustomMenuItem::new("settings".to_string(), "Settings...");
+    // // let close = CustomMenuItem::new("quit".to_string(), "Quit");
+    // let about = CustomMenuItem::new("about".to_string(), "About...");
+    // let submenu = Submenu::new(
+    //     "File",
+    //     Menu::new()
+    //         .add_item(dashboard)
+    //         .add_item(settings)
+    //         .add_native_item(MenuItem::Separator)
+    //         .add_item(about),
+    // );
+    // let menu = Menu::new()
+    //     .add_native_item(MenuItem::Copy)
+    //     .add_submenu(submenu);
 
     tauri::Builder::default()
         .manage(state)
-        .menu(menu)
+        // .menu(menu)
         .on_menu_event(|event| match event.menu_item_id() {
             "dashboard" => event.window().emit("dashboard", "").unwrap(),
             "settings" => event.window().emit("settings", "").unwrap(),
