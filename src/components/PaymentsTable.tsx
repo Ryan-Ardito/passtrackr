892b13bce5df57e5a98199799f23ce2ff3ac47a7
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-05-02 16:24:55 -0600

    scroll fix

--- src/components/PaymentsTable.tsx
+++ src/components/PaymentsTable.tsx
@@ -27,7 +27,7 @@ export function PaymentsTable() {
       value={visits}
       size="small"
       scrollable
-      style={{ height: "100%" }}
+      style={{ display: "grid", maxHeight: "100%", paddingBottom: "45px" }}
     >
       {/* <Column field="payment_id" header="ID" /> */}
       {/* <Column field="pass_id" header="Pass" /> */}
